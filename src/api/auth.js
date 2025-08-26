50fb23b016785e5f9b44b8b94c0e4c661e026980
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2025-08-26 18:48:27 +0300

    Created utils\axiosInstance.js

---
+++ src/api/auth.js
@@ -0,0 +1,7 @@
+import request from '../utils/axiosInstance';
+
+export const registerUser = data => request.post('/auth/register', data);
+
+export const loginUser = data => request.post('/auth/login', data);
+
+export const getCurrentUser = () => request.get('/auth/me');
--- src/pages/RegisterForm/RegisterForm.jsx
+++ src/pages/RegisterForm/RegisterForm.jsx
@@ -1,13 +1,52 @@
+import { useState } from 'react';
+import { registerUser } from '../../api/auth';
+
 const RegisterForm = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
+  const [message, setMessage] = useState('');
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await registerUser(form);
+      console.log('✅ Успешная регистрация:', res.data);
+    } catch (err) {
+      console.error('❌ Ошибка регистрации:', err.response?.data || err.message);
+    }
+  };
+
   return (
     <div>
-      <h2>Register Form</h2>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Register</button>
+      <h1>Регистрация</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your emai"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Create a password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Register Now</button>
       </form>
+      {message && <p>{message}</p>}
     </div>
   );
 };
