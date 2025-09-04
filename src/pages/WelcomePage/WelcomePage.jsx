21214bb9b0a652572121d7e2f77893f1202f2b4d
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2025-09-04 18:29:24 +0300

    Updated WelcomePage.module.css added styles

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,19 +1,31 @@
 import { NavLink } from 'react-router-dom';
+import Vector from '../../../public/Vector-white.svg';
+import ImageStart from '../../../public/Image-start-mob-2x.png';
+
+import css from './WelcomePage.module.css';
 
 const WelcomePage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>WelcomePage</h1>
+    <div className={css.contWelcome}>
+      <img src={ImageStart} alt="image_Start" className={css.imageStart} />
 
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', marginTop: '20px' }}>
-        <NavLink to="/auth/register">
-          <button>REGISTRATION</button>
-        </NavLink>
-
-        <NavLink to="/auth/login">
-          <button>LOG IN</button>
-        </NavLink>
+      <div className={css.contLogo}>
+        <img src={Vector} alt="Vector" className={css.Vector} />
+        <h2 className={css.title}>Task Pro</h2>
       </div>
+
+      <p className={css.text}>
+        Supercharge your productivity and take control of your tasks with Task Pro - Don't wait,
+        start achieving your goals now!
+      </p>
+
+      <NavLink to="/auth/register" className={css.btnRegLog}>
+        REGISTRATION
+      </NavLink>
+
+      <NavLink to="/auth/login" className={css.btnRegLog}>
+        LOG IN
+      </NavLink>
     </div>
   );
 };
